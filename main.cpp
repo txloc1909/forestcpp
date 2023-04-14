d816f7cf9049d72297175e1c2fa4f8fe40149dd3
Tran Xuan Loc <tranxuanloc19920@gmail.com>
2023-04-14 11:36:37 +0700

    simple Node impl

--- main.cpp
+++ main.cpp
@@ -1,6 +1,46 @@
+#include <ios>
 #include <iostream>
 
-int main(int argc, char** argv) {
-    std::cout << "Hello, world!" << std::endl;
+template<typename T>
+struct Node {
+    T value;
+    Node<T> *left;
+    Node<T> *right;
+
+    Node<T>() = delete;
+    Node<T>(T val) {
+        value = val;
+        left = right = nullptr;
+    }
+    Node<T>(T val, Node<T>* l, Node<T>* r) {
+        value = val; left = l; right = r;
+    }
+
+    void print_out() {
+        std::cout << value << ";";
+        if (left) {
+            std::cout << left->value << ";";
+        }
+        if (right) {
+            std::cout << right->value << ";";
+        }
+        std::cout << "\n";
+    };
+};
+
+int main() {
+    Node<double> a(1.2f);
+    Node<double> b(1.0f);
+    Node<double> c(2.0f, &a, &b);
+    a.print_out();
+    b.print_out();
+    c.print_out();
+
+    Node<std::string> x("xyz");
+    Node<std::string> y("abc");
+    Node<std::string> z("def", &x, &y);
+    x.print_out();
+    y.print_out();
+    z.print_out();
     return 0;
 }
