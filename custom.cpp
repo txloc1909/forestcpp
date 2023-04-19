2f48fa020c0c3f200eaf83fbc9f0619e7f1c4686
Tran Xuan Loc <tranxuanloc19920@gmail.com>
2023-04-19 12:29:10 +0700

    copy constructor and assignment for custom object

--- custom.cpp
+++ custom.cpp
@@ -2,6 +2,21 @@
 
 #include "custom.h"
 
+CustomObj::CustomObj(const CustomObj& other) {
+    id = other.id;
+    name = other.name;
+}
+
+CustomObj& CustomObj::operator=(const CustomObj &other) {
+    this->id = other.id;
+    this->name = other.name;
+    return *this;
+}
+
+bool CustomObj::operator==(const CustomObj &other) {
+    return (id == other.id) && (name == other.name);
+}
+
 std::ostream& operator<<(std::ostream& os, const CustomObj& obj) {
     os << obj.id << " | " << obj.name;
     return os;
--- custom.h
+++ custom.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <iostream>
 #include <ostream>
 #include <string>
@@ -7,9 +9,15 @@ struct CustomObj {
     std::string name;
 
     CustomObj(int id_, const std::string &name_): id(id_), name(name_) {};
+    CustomObj() : CustomObj(0, "") {};
     CustomObj(int id_) : CustomObj(id_, "") {};
     CustomObj(std::string& name) : CustomObj(0, name) {};
     CustomObj(const std::string&& name) : CustomObj(0, name) {};
+
+    CustomObj(const CustomObj& other);
+    CustomObj& operator=(const CustomObj& other);
+
+    bool operator==(const CustomObj& other);
 };
 
 std::ostream& operator<<(std::ostream& os, const CustomObj& obj);
